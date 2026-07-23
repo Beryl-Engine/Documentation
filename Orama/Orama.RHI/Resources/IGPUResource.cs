024853c6cde3ec932bfe552711ad31cdc28b2a9e
Tweety <allie.g@tweety-lab.dev>
2026-07-23 18:29:02 +1000

    Remove orama.

--- Orama/Orama.RHI/Resources/IGPUResource.cs
+++
@@ -1,8 +0,0 @@
-// This file is part of the Orama Game Engine.
-// Licensed under the MIT license. (https://github.com/Orama-Engine/Orama/blob/main/LICENSE)
-
-namespace Orama.RHI.Resources;
-
-public interface IGPUResource : IDisposable
-{
-}
